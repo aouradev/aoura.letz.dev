44876618fd76b2db864dbb735d3e9081738c4c16
aoura <Jepssy06@gmail.com>
2024-06-06 22:44:43 -0600

    yes 2

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        var loader = document.querySelector('.container');
-        var homepage = document.querySelector('.homepage');
-
-        loader.style.animation = 'fadeOut 0.7s forwards'; // Apply fade out animation
-        homepage.classList.add('visible'); // Show homepage after loader fades out
-    }, 700);
-});
